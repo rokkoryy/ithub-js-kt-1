959965de451126833e72487cc774428cd72178f9
Ковалев Роман <r0m4.k0v@gmail.com>
2025-09-08 13:59:08 +0300

    task - random

---
+++ task-random.js
@@ -0,0 +1,8 @@
+const minNumber = 3
+const maxNumber = 9
+const result = Math.round(math.random() * (maxNumber - minNumber)) + minNumber
+
+console.log('случайное число в диапазоне [', minNumber, maxNumber, ']  ', result)
+
+
+
